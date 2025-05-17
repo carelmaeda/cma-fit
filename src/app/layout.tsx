679d8bc8037329549f187607810d38d8fab6bcf3
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-05-17 10:21:37 -0400

    fix: bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ReactNode } from 'react';
 import './styles/global.scss';
 import Script from 'next/script';
-import { ReactNode } from 'react';
 
 
 export const metadata = {
@@ -14,19 +13,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        {/* Bootstrap & Popper.js */}
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          strategy="beforeInteractive"
-        />
       </head>
-      <body>
-        {children}
+      <body>{children}
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossOrigin="anonymous"></Script>
       </body>
     </html>
   );
