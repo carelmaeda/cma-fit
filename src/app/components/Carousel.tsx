9abceac9814f1d462b0f1d9db1b45470a1821818
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-05-17 10:51:45 -0400

    feat: marquee

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const Carousel: React.FC = () => {
-  const words = ["Bodybuilding", "Coaching", "Personal Trainer", "Fitness Plan", "Workout Routine", "Nutrition"];
-
+  const words = ["Bodybuilding", "Coaching", "Personal Trainer", "Fitness Plan", "Workout Routine", "Nutrition", "Life Changing"];
+  
   return (
-    <section className="carousel-wrapper">
-          {words.map((word, index) => (
-            <div key={index} >
-              {word}
-            </div>
-          ))}
-    </section>
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        <div className="marquee">
+          <div className="marquee-content">
+            {[...Array(4)].map((_, i) => (
+              <React.Fragment key={i}>
+                {words.map((word, index) => (
+                  <span key={`${word}-${i}-${index}`} className="marquee-word">
+                    {word}
+                  </span>
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
