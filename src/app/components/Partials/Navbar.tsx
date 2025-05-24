d76281bd8a802e5d4a3720138866e9b34f0fd85b
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-05-23 21:15:43 -0400

    fix: navbar sizing

--- src/app/components/Partials/Navbar.tsx
+++ src/app/components/Partials/Navbar.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { MoveRight } from "lucide-react";
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,12 +15,15 @@ const Navbar: React.FC = () => {
     <nav>
       <div className="navbar">
         <div className="nav-header">
-          <Link href="/" className="logo">JUSTIN LYONS</Link>
+          <Link href="/" className="logo">         
+             <Image src="/images/jl-logo.png" alt="Justin Lyons Logo" width={746} height={100} />
+           </Link>
           <button
             className={`menu-toggle ${menuOpen ? 'open' : ''}`}
             onClick={toggleMenu}
             aria-label="Toggle navigation"
           >
+            <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
           </button>
