60aa28195e2c9f3bf7836d0e8c229e63cea1a5e3
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-06-17 09:08:24 -0400

    fix: cosmetic

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -13,15 +13,11 @@ const About: React.FC = () => {
               height={400}
             />
           <div className="about-text">
+            <p>We need Justin's Bio here</p>
             <p>
-             Whether you are starting your fitness journey or aiming for the next
-level, this training program is designed to help you reach your full
-potential through a structured approach that builds progressively over
-time. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel venenatis mi. Mauris ultrices molestie nibh ac venenatis. Donec placerat semper urna. Donec gravida, massa ut rutrum ullamcorper, nulla enim volutpat felis.
             </p>
-            <p>I have created a four-phase system that will guide you every step
-of the way, from establishing a solid foundation to achieving peak
-performance.</p>
+            <p>Sed dignissim tellus ligula, nec lacinia tortor efficitur id. Nam commodo tellus elementum magna posuere vehicula. Donec commodo faucibus pretium. Mauris volutpat urna vel risus mattis hendrerit</p>
           </div>
         </div>
     </section>
