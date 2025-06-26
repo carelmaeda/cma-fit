3aeb02d6dc676fb42fd38158d616432657c5a8ea
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-06-26 09:51:23 -0400

    feat: footer

--- src/app/components/Partials/Footer.tsx
+++ src/app/components/Partials/Footer.tsx
@@ -1,42 +1,35 @@
 import React from 'react';
-import { Facebook, Instagram } from 'lucide-react';
+import { Instagram } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer-section container-fluid">
-        <div className="row text-center text-md-start">
-          <div className="col-md-4 mb-4">
-            <h4 className="footer-brand">Justin Lyons</h4>
-            <p>
-              Your journey to peak performance starts here. Empower your body.
-              Elevate your life.
-            </p>
+    <footer className="footer-section">
+          <div className="footer-brand">
+          <Link href="/" className="logo">         
+            <Image src="/images/jl-logo-white.png" alt="Justin Lyons Logo" width={746} height={100} />
+          </Link>
+            <p>My simple 3-month program will get you fit, strong, and confident.</p>
           </div>
 
-          <div className="col-md-4 mb-4">
+          <div className="footer-links">
             <h5>Quick Links</h5>
-            <ul className="list-unstyled">
+            <ul className="">
               <li><a href="#about">About</a></li>
-              <li><a href="#program">Program</a></li>
-              <li><a href="#testimonials">Testimonials</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li><a href="#theprogram">Program</a></li>
+              <li><a href="#audience">Testimonials</a></li>
+              <li><a href="#contact">Book a Call</a></li>
             </ul>
           </div>
 
-          <div className="col-md-4 mb-4">
-            <h5>Follow Me</h5>
             <div className="footer-social">
-              <a href="#"><Facebook 
-              size={32}/></a>
-              <a href="#"><Instagram 
-              size={32}/></a>
-
+              <h4>Let's Connect</h4>
+              <a href="https://www.instagram.com/the.lyonsking" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><Instagram size={32} /></a>
             </div>
-          </div>
-        </div>
 
-        <div className="text-center pt-4 border-top mt-4">
+        <div className="footer-bottom">
           <small>© {new Date().getFullYear()} Justin Lyons. All rights reserved.</small>
         </div>
     </footer>
