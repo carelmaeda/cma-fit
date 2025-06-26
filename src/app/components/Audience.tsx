df8a317c02fba21ac936686f737c191631c785a4
Carel Maeda Ambrosio <carel.maeda@paygos.ca>
2025-06-26 09:13:34 -0400

    fix; quotes audience

--- src/app/components/Audience.tsx
+++ src/app/components/Audience.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-
+import { Quote } from 'lucide-react';
 import React from 'react';
 
 
 const Audience: React.FC = () => {
   return (
     <section className="audience-section">
+        <div className='quote1'><Quote /></div>
         <h3>This program is designed for anyone ready to take control of their fitness — from total beginners to returning athletes.</h3>
+        <div className='quote2'><Quote /></div>
     </section>
   );
 };
